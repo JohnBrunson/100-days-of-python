195da9f9573ef880898d10f4ce7ecf2f438ff7f2
John Brunson <JohnBrunson2009@gmail.com>
2023-03-14 20:40:29 -0700

    trying again...

---
+++ day-7/hangman/day-2.py
@@ -0,0 +1,31 @@
+import random
+import logging
+
+# initialization
+
+word_list = ['aardvark', 'baboon', 'camel']
+chosen_word = random.choice(word_list)
+logging.info(f'Chosen word is: {chosen_word}')
+display = []
+word_length = len(chosen_word)
+
+# Extra Credit -- logging
+logging.basicConfig(filename='hangman.log', encoding='utf-8',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+# display the blanks to the user
+for _ in range(word_length):
+    display += '_'
+print(display)
+
+# prompt the user for a guess
+guess = input('Enter a letter: ').lower()
+
+logging.info(f'User input for guess was {guess}')
+
+# evaluate guess and display result to user
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+print(display)
