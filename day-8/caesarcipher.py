69a58583d01045b18878d24146240ded3454b9a8
John Brunson <JohnBrunson2009@gmail.com>
2023-03-30 18:00:08 -0700

    improvements made. still probably clunky.

--- day-8/caesarcipher.py
+++ day-8/caesarcipher.py
@@ -6,7 +6,10 @@ import string
 #    alphabet = list(string.ascii_lowercase)
 #    return (alphabet)
 
+# I confess, I don't like this solution. But I'm not sure about how to get a better one yet.
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 # UI
 
@@ -18,30 +21,40 @@ def main_menu():
     3. Exit 
     ''')
     if direction == 1:
+        text_functions()
         encrypt()
-    # if direction == 2:
-
+    if direction == 2:
+        print("This function doesn't work yet.")
+        main_menu()
     if direction == 3:
         quit()
+
+
+def encrypt(plain_text, shift_amount):
+    for letter in plain_text:
+        # get the letter shifted by the shift number
+        encrypted_letter = (shift_amount + (alphabet.index(letter)))
+        # append the letter to the encrypted alphabet
+        encrypted_alphabet.append(alphabet[int(encrypted_letter)])
+        # take the list and join into one string
+        encrypted_text = ''.join(encrypted_alphabet)
+    print(encrypted_text)
 
 
 # Global Varibles
 text = input('Enter the message: ').lower()
 shift = int(input("Enter the shift number: "))
 encrypted_alphabet = []
+encrypt(plain_text=text, shift_amount=shift)
 # Functions
 
-for letter in text:
-    # get the letter shifted by the shift number
-    encrypted_letter = (shift + (alphabet.index(letter)))
-    # append the letter to the encrypted alphabet
-    encrypted_alphabet.append(alphabet[int(encrypted_letter)])
-    # take the list and join into one string
-    encrypted_text = ''.join(encrypted_alphabet)
-    print(encrypted_text)
+
 # print(encrypted_alphabet)
 # def encrypt(text, shift):
 #    encrypted.alphabet = ""
+
+
+# Temporary main
 
 
 # TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
