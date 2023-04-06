be7e5f9d148f5aef68655e4e385fa3a7be38bdae
John Brunson <JohnBrunson2009@gmail.com>
2023-04-05 20:22:59 -0700

    :refactor completeA

--- day-8/caesarcipherrefactor.py
+++ day-8/caesarcipherrefactor.py
@@ -1,5 +1,6 @@
 # Libraries Imported
 import string
+import art
 
 # Function to generate the alphabet using ASCII characters.
 # def generate_alphabet():
@@ -12,7 +13,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 # Global Variables
-calculated_alphabet = []
+
 # UI
 
 
@@ -25,32 +26,60 @@ def main_menu():
     if cipher_direction == '1':
         text = input('Enter the message: ').lower()
         shift = int(input("Enter the shift number: "))
+        # accounts for numbers larger than 26.
+        shift = shift % 26
         # encrypt(plain_text=text, shift_amount=shift)
         caesar(input_text=text, shift_amount=shift, direction=cipher_direction)
-    if cipher_direction == '2':
+    elif cipher_direction == '2':
         text = input('Enter the message: ').lower()
         shift = int(input("Enter the shift number: "))
-        # decrypt(cipher_text=text, shift_amount=shift)
+        # accounts for numbers larger than 26.
+        shift = shift % 26
         caesar(input_text=text, shift_amount=shift, direction=cipher_direction)
-    if cipher_direction == '3':
+    elif cipher_direction == '3':
         quit()
+    else:
+        print("That is an invalid selection. Please try again.")
+        main_menu()
 
 
 def caesar(input_text, shift_amount, direction):
+    print('Please note, all input will be converted to lower case. All non alphabetic input will not be encoded.')
+    calculated_alphabet = ''
     for letter in input_text:
+        if letter not in alphabet:
+            calculated_alphabet += letter
         # get the letter shifted by the shift number
-        if direction == '1':
+        elif direction == '1' and letter in alphabet:
             calculated_letter = (shift_amount + (alphabet.index(letter)))
-        elif direction == '2':
+            calculated_alphabet += (alphabet[int(calculated_letter)])
+        elif direction == '2' and letter in alphabet:
             calculated_letter = ((alphabet.index(letter)) - shift_amount)
-        # append the letter to the encrypted alphabet
-        calculated_alphabet.append(alphabet[int(calculated_letter)])
-        # take the list and join into one string
-        calculated_text = ''.join(calculated_alphabet)
+            calculated_alphabet += (alphabet[int(calculated_letter)])
+
     if direction == '1':
-        print(f'The encoded text is: {calculated_text}')
+        print(f'The encoded text is: {calculated_alphabet}')
     elif direction == '2':
-        print(f'The decoded text is: {calculated_text}')
+        print(f'The decoded text is: {calculated_alphabet}')
+    # cleanup
+    calculated_letter = ''
+    calculated_alphabet = ''
+    # restart the program?
+    cipher_restart()
 
 
+def cipher_restart():
+    answer = input(
+        "Do you want to restart the cipher program (y or n)?").lower()
+    if answer == 'y':
+        main_menu()
+    elif answer == 'n':
+        print('Thank you for using Caesar Cipher!')
+        quit()
+    else:
+        print('You have used invalid input. Please try again.')
+        cipher_restart()
+
+
+print(art.logo)
 main_menu()
